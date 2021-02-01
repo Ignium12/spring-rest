22e96e3b84b22325551ecfde464410125e8cd947
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-01 13:53:54 +0100

    Refactored code to support global exception handling

--- src/main/java/com/kruehl/spring/rest/StudentRestController.java
+++ src/main/java/com/kruehl/spring/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.kruehl.spring.rest;
 
 import com.kruehl.spring.entity.Student;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -50,35 +51,4 @@ public class StudentRestController {
         return students.get(studentId);
     }
 
-    // Add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
