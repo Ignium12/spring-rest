fefa93672166bdacb31c2fe31d796485e9e63493
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-01 13:28:49 +0100

    Addded exception handling for invalid numbers when using /api/students/{studenId}

--- src/main/java/com/kruehl/spring/rest/StudentRestController.java
+++ src/main/java/com/kruehl/spring/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.kruehl.spring.rest;
 
 import com.kruehl.spring.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -41,6 +40,31 @@ public class StudentRestController {
 
         // just index into the list...keep it simple for now
 
+        // check the studentId against list size
+
+        if((studentId >= students.size()) || (studentId <0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+
         return students.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+
+        // create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
     }
 }
